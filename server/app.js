2c31db308c4e9ca2ade013041471382cb910cbf8
Dev-fun-123 <doductrung.2023@gmail.com>
2024-11-21 22:06:14 +0700

    Update

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+
+// Sử dụng cổng từ biến môi trường hoặc mặc định là 3000
+const port = process.env.PORT || 3000;
 
 // Cung cấp các file tĩnh như HTML
 app.use(express.static(path.join(__dirname, '../public')));
@@ -12,7 +14,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// Lắng nghe yêu cầu trên cổng 3000
+// Lắng nghe yêu cầu trên cổng mà Render cung cấp (hoặc 3000 nếu không có)
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
