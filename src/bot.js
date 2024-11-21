11bffcfcbd7022969ed42428ab1f6f4951a2cb32
Dev-fun-123 <doductrung.2023@gmail.com>
2024-11-21 21:53:13 +0700

    first commit

---
+++ src/bot.js
@@ -0,0 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '7610000015:AAG5av5x8Zno4wPQBtU33rbXZA1KCyr7Nv8';
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Welcome! Please connect your wallet using this link: https://<YOUR_APP_URL>');
+});
